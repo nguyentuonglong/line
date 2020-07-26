a8345e744865e9d070e559fdce4eb70be67abd6a
Long Nguyen <long.nguyen@mingle.com>
2020-07-26 15:27:10 +0700

    [f] change the way to load image

--- app/src/main/java/vn/com/line/linedemo/MainActivity.kt
+++ app/src/main/java/vn/com/line/linedemo/MainActivity.kt
@@ -1,18 +1,19 @@
 package vn.com.line.linedemo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.model.GlideUrl
 import okhttp3.Interceptor
-import okhttp3.Response
-import timber.log.Timber
+import okhttp3.OkHttpClient
 import vn.com.line.linedemo.databinding.ActivityMainBinding
-import vn.com.line.linedemo.network.ApiService
 import vn.com.line.linedemo.util.GlideApp
 import vn.com.line.linedemo.util.ProgressListener
 import vn.com.line.linedemo.util.ProgressResponseBody
-import java.io.IOException
+import java.io.InputStream
 
 
 class MainActivity : AppCompatActivity() {
@@ -22,35 +23,43 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-//        val progressListener: ProgressListener = object : ProgressListener {
-//            override fun update(bytesRead: Long, contentLength: Long, done: Boolean) {
-//                val progress = (100 * bytesRead / contentLength).toInt()
-//
-//                // Enable if you want to see the progress with logcat
-//                // Log.v(LOG_TAG, "Progress: " + progress + "%");
-//                binding.progressBar.progress = progress
-//                if (done) {
-//                    Timber.d("Done loading")
-//                }
-//            }
-//        }
-//
-//        ApiService.getOkHttpClient().toMutableList().add(object : Interceptor {
-//            @Throws(IOException::class)
-//            override fun intercept(chain: Interceptor.Chain): Response {
-//                val originalResponse: Response = chain.proceed(chain.request())
-//                return originalResponse.newBuilder()
-//                    .body(ProgressResponseBody(originalResponse.body, progressListener))
-//                    .build()
-//            }
-//        })
-//        Thread.sleep(3000)
+        init()
         GlideApp.with(this)
             .load("https://i.imgur.com/mYBXl6X.jpg")
             // Disabling cache to see download progress with every app load
             // You may want to enable caching again in production
             .diskCacheStrategy(DiskCacheStrategy.NONE)
+            .skipMemoryCache(true)
             .into(binding.ivImage)
+    }
+
+    private fun init() {
+        val mOkHttpClient = OkHttpClient()
+        val progressListener =
+            object : ProgressListener {
+                override fun update(bytesRead: Long, contentLength: Long, done: Boolean) {
+                    val progress = (100 * bytesRead / contentLength).toInt()
+                    // Enable if you want to see the progress with logcat
+                    Log.v("LOG_TAG", "Progress: $progress%")
+                    if (done) {
+                        Log.i("LOG_TAG", "Done loading")
+                    }
+                }
+            }
+
+        val builder: OkHttpClient.Builder
+        builder = mOkHttpClient.newBuilder()
+        builder.addInterceptor(Interceptor { chain: Interceptor.Chain ->
+            val originalResponse = chain.proceed(chain.request())
+            originalResponse.newBuilder()
+                .body(ProgressResponseBody(originalResponse.body, progressListener))
+                .build()
+        })
+
+        GlideApp.get(this).registry.replace(
+            GlideUrl::class.java,
+            InputStream::class.java,
+            OkHttpUrlLoader.Factory(builder.build())
+        )
     }
 }
--- app/src/main/java/vn/com/line/linedemo/util/LineAppGlideModule.java
+++ app/src/main/java/vn/com/line/linedemo/util/LineAppGlideModule.java
@@ -1,27 +1,13 @@
 package vn.com.line.linedemo.util;
 
-import android.content.Context;
-import android.util.Log;
-
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
-import com.bumptech.glide.load.model.GlideUrl;
 import com.bumptech.glide.module.AppGlideModule;
 
-import org.jetbrains.annotations.NotNull;
-
-import java.io.InputStream;
-
 import okhttp3.OkHttpClient;
-import okhttp3.Response;
 
 
 @GlideModule
 public class LineAppGlideModule extends AppGlideModule {
-
-    private OkHttpClient mOkHttpClient;
 
 
 //    @Override
@@ -32,35 +18,6 @@ public class LineAppGlideModule extends AppGlideModule {
 //        builder.setDefaultRequestOptions(requestOptions());
 //        builder.build(context);
 //    }
-
-    @Override
-    public void registerComponents(@NotNull Context context, Glide glide, @NotNull Registry registry) {
-        mOkHttpClient = new OkHttpClient();
-
-        final ProgressListener progressListener = (bytesRead, contentLength, done) -> {
-            int progress = (int) ((100 * bytesRead) / contentLength);
-            // Enable if you want to see the progress with logcat
-            Log.v("LOG_TAG", "Progress: " + progress + "%");
-            if (done) {
-                Log.i("LOG_TAG", "Done loading");
-            }
-        };
-
-        OkHttpClient.Builder builder;
-        if (mOkHttpClient != null) {
-            builder = mOkHttpClient.newBuilder();
-        } else {
-            builder = new OkHttpClient.Builder();
-        }
-        builder.addInterceptor(chain -> {
-            Response originalResponse = chain.proceed(chain.request());
-            return originalResponse.newBuilder()
-                    .body(new ProgressResponseBody(originalResponse.body(), progressListener))
-                    .build();
-        });
-        glide.getRegistry().replace(GlideUrl.class, InputStream.class,
-                new OkHttpUrlLoader.Factory(builder.build()));
-    }
 
 //    private static RequestOptions requestOptions(){
 //        return new RequestOptions()
