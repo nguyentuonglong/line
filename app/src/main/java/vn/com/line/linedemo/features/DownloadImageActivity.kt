7f114658130b196b0343b8a6a4220481ead0e350
Long Nguyen <long.nguyen@mingle.com>
2020-07-27 15:24:51 +0700

    [f] Change logic into viewModel

---
+++ app/src/main/java/vn/com/line/linedemo/features/DownloadImageActivity.kt
@@ -0,0 +1,141 @@
+package vn.com.line.linedemo.features
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import vn.com.line.linedemo.R
+import vn.com.line.linedemo.base.BaseActivity
+import vn.com.line.linedemo.databinding.ActivityMainBinding
+import vn.com.line.linedemo.viewmodel.DownloadImageViewModel
+
+
+class DownloadImageActivity : BaseActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel by viewModels<DownloadImageViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,
+            R.layout.activity_main
+        )
+        init()
+        setUpViewModels()
+        val buildVersion = Build.VERSION.SDK_INT
+        if (buildVersion >= Build.VERSION_CODES.M) {
+            if (!checkPermission()) {
+                requestPermission()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkPermission()) {
+                viewModel.checkLocalImageFolder()
+            } else {
+                requestPermission()
+            }
+        } else {
+            viewModel.checkLocalImageFolder()
+        }
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            STORAGE_REQUEST_PERMISSION
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            STORAGE_REQUEST_PERMISSION -> if (grantResults.isNotEmpty()) {
+
+                val cameraAccepted = grantResults.first() == PackageManager.PERMISSION_GRANTED
+                if (!cameraAccepted) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                            requestPermissions(
+                                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                                STORAGE_REQUEST_PERMISSION
+                            )
+                        } else {
+                            Toast.makeText(
+                                applicationContext,
+                                getString(R.string.permissions_denied),
+                                Toast.LENGTH_LONG
+                            ).show()
+                            //Start setting
+                            val intent = Intent()
+                            intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                            val uri = Uri.fromParts("package", packageName, null)
+                            intent.data = uri
+                            this.startActivity(intent)
+                            this.finish()
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun checkPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            applicationContext,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun setUpViewModels() {
+        viewModel.downloadState.observe(this, Observer { state ->
+            binding.progressContainer.isVisible =
+                state == DownloadImageViewModel.DownloadState.STATE_STARTED
+        })
+
+        viewModel.progressDownload.observe(this, Observer { downloadedKb ->
+            val downloadedAsString =
+                String.format(getString(R.string.downloaded_holder), downloadedKb)
+            binding.tvDownloaded.text = downloadedAsString
+        })
+
+        viewModel.imageBitmap.observe(this, Observer { bitmap ->
+            binding.ivImage.setImageBitmap(bitmap)
+        })
+
+        viewModel.movieTitle.observe(this, Observer {
+            binding.tvTitle.text = it
+        })
+        viewModel.isOutOfImages.observe(this, Observer { isOut ->
+            if (isOut) Toast.makeText(this, getString(R.string.out_of_image), Toast.LENGTH_SHORT)
+                .show()
+        })
+    }
+
+    private fun init() {
+        binding.ivImage.setOnClickListener {
+            viewModel.fetchNextImage()
+        }
+    }
+
+    companion object {
+        private const val STORAGE_REQUEST_PERMISSION = 7979
+    }
+}
--- app/src/main/java/vn/com/line/linedemo/viewmodel/DownloadImageViewModel.kt
+++ app/src/main/java/vn/com/line/linedemo/viewmodel/DownloadImageViewModel.kt
@@ -10,6 +10,7 @@ import io.reactivex.Completable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import vn.com.line.linedemo.LineApp
+import vn.com.line.linedemo.network.model.Movie
 import java.io.*
 import java.net.HttpURLConnection
 import java.net.URL
@@ -18,21 +19,22 @@ import java.util.*
 
 class DownloadImageViewModel(application: Application) : AndroidViewModel(application) {
 
-    val donwloadState = MutableLiveData<DownloadState>()
+    val downloadState = MutableLiveData<DownloadState>()
     val progressDownload = MutableLiveData<Int>()
-    val imagePath = MutableLiveData<String>()
-    val bitmapFromPath = MutableLiveData<Bitmap>()
-
-    //TO save local
-    private val bitmapStore = MutableLiveData<List<Bitmap>>()
+    val imageBitmap = MutableLiveData<Bitmap>()
+    val movieTitle = MutableLiveData<String>()
+    val isOutOfImages = MutableLiveData<Boolean>()
+    private var movieData: Movie? = null
+    private var indexOfImage = 0
+    private var isDownloaded = false
 
     enum class DownloadState {
         STATE_STARTED, STATE_FAILED, STATE_ENDED
     }
 
-    fun downloadImage(imageUrl: String) {
+    private fun downloadImage(imageUrl: String) {
         Completable.fromAction {
-            donwloadState.postValue(DownloadState.STATE_STARTED)
+            downloadState.postValue(DownloadState.STATE_STARTED)
             var inputStream: InputStream? = null
             var os: OutputStream? = null
             var con: HttpURLConnection? = null
@@ -42,7 +44,7 @@ class DownloadImageViewModel(application: Application) : AndroidViewModel(applic
                 con = url.openConnection() as? HttpURLConnection
                 con?.connect()
                 if (con?.responseCode != HttpURLConnection.HTTP_OK) {
-                    donwloadState.postValue(DownloadState.STATE_FAILED)
+                    downloadState.postValue(DownloadState.STATE_FAILED)
                     return@fromAction
                 }
                 length = con.contentLength
@@ -63,8 +65,12 @@ class DownloadImageViewModel(application: Application) : AndroidViewModel(applic
                     }
                     os.write(data, 0, count)
                 }
-                imagePath.postValue(path)
-                donwloadState.postValue(DownloadState.STATE_ENDED)
+                val file = File(path)
+                if (file.exists()) {
+                    val bitmap = BitmapFactory.decodeFile(file.absolutePath)
+                    imageBitmap.postValue(bitmap)
+                }
+                downloadState.postValue(DownloadState.STATE_ENDED)
             } catch (e: Exception) {
                 e.printStackTrace()
             } finally {
@@ -81,21 +87,82 @@ class DownloadImageViewModel(application: Application) : AndroidViewModel(applic
 
     }
 
-    fun loadImageFromPath(path: String) {
+    private fun loadLocalImage() {
         Completable.fromAction {
-            val file = File(path)
-            if (file.exists()) {
-                val bitmap = BitmapFactory.decodeFile(file.absolutePath)
-                bitmapFromPath.postValue(bitmap)
+            movieData?.image?.let { list ->
+                if (indexOfImage < list.size) {
+                    val path = list[indexOfImage] ?: ""
+                    if (path.isNotEmpty()) {
+                        val file = File(path)
+                        if (file.exists()) {
+                            val bitmap = BitmapFactory.decodeFile(file.absolutePath)
+                            indexOfImage++
+                            imageBitmap.postValue(bitmap)
+                        }
+                    }
+                } else {
+                    isOutOfImages.postValue(true)
+                }
             }
         }.subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe()
     }
 
+    private fun loadRemoteImage() {
+        Completable.fromAction {
+            movieData?.image?.let { list ->
+                if (indexOfImage < list.size) {
+                    val url = movieData?.image?.get(indexOfImage) ?: ""
+                    if (url.isNotEmpty()) {
+                        downloadImage(url)
+                        indexOfImage++
+                    }
+                } else {
+                    isOutOfImages.postValue(true)
+                }
+            }
+
+        }.subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe()
+    }
+
+    fun checkLocalImageFolder() {
+        val fileDirectory =
+            File(
+                getApplication<LineApp>().getDir(DIRECTORY_OUTPUTS, Context.MODE_PRIVATE)
+                    .toString()
+            )
+        val dirFiles = fileDirectory.listFiles()
+        if (dirFiles != null && dirFiles.isNotEmpty()) {
+            val listOfPath = dirFiles.map {
+                it.absolutePath
+            }
+            movieData = Movie(title = "Civil War", image = listOfPath)
+            movieTitle.postValue(movieData?.title)
+        } else {
+            genDummyData()
+        }
+        isDownloaded = dirFiles != null && dirFiles.isNotEmpty()
+        fetchNextImage()
+    }
+
+    fun fetchNextImage() {
+        if (isDownloaded) loadLocalImage() else loadRemoteImage()
+    }
+
+    private fun genDummyData() {
+        val listOfImage = listOf(
+            "http://movie.phinf.naver.net/20151127_272/1448585271749MCMVs_JPEG/movie_image.jpg?type=m665_443_2",
+            "http://movie.phinf.naver.net/20151127_84/1448585272016tiBsF_JPEG/movie_image.jpg?type=m665_443_2",
+            "http://movie.phinf.naver.net/20151125_36/1448434523214fPmj0_JPEG/movie_image.jpg?type=m665_443_2"
+        )
+        movieData = Movie(title = "Civil War", image = listOfImage)
+        movieTitle.postValue(movieData?.title)
+    }
+
     companion object {
         private const val DIRECTORY_OUTPUTS = "outputs"
     }
-
-
 }
